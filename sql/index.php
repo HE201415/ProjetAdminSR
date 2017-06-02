4ff8bca696cf5fff459119330e8331f3e12e0f7b
HE201415 <HE201415@students.ephec.be>
2017-06-02 10:47:01 +0200

    Update index.php

--- sql/index.php
+++ sql/index.php
@@ -6,7 +6,7 @@
         $dbh = new PDO('mysql:host=172.17.0.3;dbname=prods','juhani','ephec');
                 $donnee = $dbh->query("select * from produit");
                                 $donnee->execute();
-                                echo($donnee->fetchAll(PDO::FETCH_ASSOC));
+                                print_r($donnee->fetchAll(PDO::FETCH_ASSOC));
     }
     catch (PDOException $e) {
         print ("Error : " . $e->getMessage() . "<br/>");
